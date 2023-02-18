bbd92f6a6c08a327d642b95c548cae66bfb9160c
Adebisi Tosin <79862233+certifiedTboy@users.noreply.github.com>
2023-02-18 21:22:58 +0100

    Update 5-printf.c
    5-printf task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
-/*
-* main - entry point
-*
-* return: Always 0
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
 */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
