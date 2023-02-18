263cebd2c0673271add4bc0a1b4ea8eb6cc90ed6
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2023-02-18 22:29:06 +0100

    101-quotes task

---
+++ 0x00-hello_world/101-quotes.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Return: 1 (Error)
+*/
+
+int main(void)
+{
+    write(2, "and that pieceof art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+    return (1)
+}
