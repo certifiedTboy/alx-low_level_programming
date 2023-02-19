098d9628b250f33d13b5b9f97a1c6d1d705c9696
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2023-02-19 09:26:28 +0100

    0-negative_positive task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
 int n;
