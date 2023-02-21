f2116b4abf5deda9c42bc65951b2ca890965a5ec
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2023-02-21 18:56:38 +0100

    1-print_alphanet task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+print_alphabet()
+return (0);
+}
+
+void print_alphabet()
+{
+char chars;
+for (chars = 'a'; chars <= 'z'; chars++)
+{
+putchar(chars);
+}
+putchar('\n');
+}
