5c8ed8808706d5ccc603fe7309488e6bf59a92b0
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2023-02-24 11:26:17 +0100

    debugging task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,14 @@
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
-int a, b, c;
-int largest;
-a = 972;
-b = -98;
-c = 0;
-largest = largest_number(a, b, c);
-printf("%d is the largest number\n", largest);
-
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
 }
